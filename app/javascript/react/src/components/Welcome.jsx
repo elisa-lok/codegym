2e54a0f43e7d480502602b90dd8eeb6660685703
Elisa <elisalok413@gmail.com>
2024-04-19 20:04:22 +1200

    change function to class component

--- app/javascript/react/src/components/Welcome.jsx
+++ app/javascript/react/src/components/Welcome.jsx
@@ -3,14 +3,16 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import QuestionList from './QuestionList';
 
-const Welcome = () => {
-  return (
-    <div className='container'>
-      <h1>Hello World! Welcome to CodeGym.</h1>
-      <p className="lead">this is the first lecture.</p>
-      <QuestionList />
-    </div>
-  )
+class Welcome extends React.Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1>Hello World! Welcome to CodeGym.</h1>
+        <p className="lead">this is the first lecture.</p>
+        <QuestionList />
+      </div>
+    )
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
