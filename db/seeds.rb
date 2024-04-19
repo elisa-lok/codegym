2b78fa36e6a9e47011d4356a82e3b054ff41d5b6
Elisa <elisalok413@gmail.com>
2024-04-19 21:00:42 +1200

    import data via db:seed and show json data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,47 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Creating Questions'
+questions = Question.create([
+  {
+    title: 'How to check if a key is present in a hash?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'What is the difference between strings and symbol?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'What happen if you add two same keys in hash?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'How to delete a key from a hash?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'How to check if two hashes are identical?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'How to combine two hashes in Ruby?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'How to get unique keys from two hashes in Ruby?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'How does the has_key? key? member? and include? methods in a hash?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'What are blocks in Ruby?',
+    tag: 'Ruby'
+  },
+  {
+    title: 'Does the order of keys matters to compare two hashes in Ruby?',
+    tag: 'Ruby'
+  }
+])
+puts 'Questions Created'
