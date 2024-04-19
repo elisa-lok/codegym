7017f60589a6ffd7c143ec3edf16107ec94142ed
Elisa <elisalok413@gmail.com>
2024-04-19 20:23:50 +1200

    change function component to class component

--- app/javascript/react/src/components/QuestionDetail.jsx
+++ app/javascript/react/src/components/QuestionDetail.jsx
@@ -1,22 +1,53 @@
 import * as React from 'react';
-import { useState } from 'react';
+//import { useState } from 'react';
 import * as ReactDOM from 'react-dom';
 
-const QuestionDetail = (props) => {
-  const [likeCount, setLikeCount] = useState(0);
+class QuestionDetail extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { likeCount: 0 }
+    this.updateLikeCounter = this.updateLikeCounter.bind(this)
+  }
 
-  return (
-    <div className='card rounded-0 mt-3'>
-      <div className='card-body'>
-        <h3 className='card-title'>{props.question.title}</h3>
-        <p className='lead'>
-          <span className='badge bg-primary'>{props.question.tag}</span>
-        </p>
-        <button className='btn btn-primary mt-1' onClick={() => setLikeCount(likeCount + 1)}>Like</button>
-        {likeCount > 0 ? <span className='badge bg-primary'>{likeCount}</span> : ''}     
+  updateLikeCounter() {
+    this.setState(function(state) {
+      return {
+        likeCount: state.likeCount + 1
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div className='card rounded-0 mt-3'>
+        <div className='card-body'>
+          <h3 className='card-title'>{this.props.question.title}</h3>
+          <p className='lead'>
+            <span className='badge bg-primary'>{this.props.question.tag}</span>
+          </p>
+          <button className='btn btn-primary mt-1' onClick={this.updateLikeCounter}>Like</button>
+          {this.state.likeCount > 0 ? <span className='badge bg-primary'>{this.state.likeCount}</span> : ''}     
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+// const QuestionDetail = (props) => {
+//   const [likeCount, setLikeCount] = useState(0);
+
+//   return (
+//     <div className='card rounded-0 mt-3'>
+//       <div className='card-body'>
+//         <h3 className='card-title'>{props.question.title}</h3>
+//         <p className='lead'>
+//           <span className='badge bg-primary'>{props.question.tag}</span>
+//         </p>
+//         <button className='btn btn-primary mt-1' onClick={() => setLikeCount(likeCount + 1)}>Like</button>
+//         {likeCount > 0 ? <span className='badge bg-primary'>{likeCount}</span> : ''}     
+//       </div>
+//     </div>
+//   )
+// }
 
 export default QuestionDetail
