f5780d6ca1a193363990a43b9c458a8746fac7b1
Elisa <elisalok413@gmail.com>
2024-04-21 19:42:58 +1200

    create api to fetch data

--- app/javascript/react/src/components/QuestionList.jsx
+++ app/javascript/react/src/components/QuestionList.jsx
@@ -1,60 +1,77 @@
 import * as React from 'react';
+import { useState, useEffect } from 'react';
 import * as ReactDOM from 'react-dom';
 import QuestionDetail from './QuestionDetail';
 
 const QuestionList = () => {
-  const questionsList = [
-    {
-      id: 1,
-      title: 'How to check if a key is present in a hash?',
-      tag: 'Ruby'
-    },
-    {
-      id: 2,
-      title: 'What is the difference between strings and symbol?',
-      tag: 'Ruby'
-    },
-    {
-      id: 3,
-      title: 'What happen if you add two same keys in hash?',
-      tag: 'Ruby'
-    },
-    {
-      id: 4,
-      title: 'How to delete a key from a hash?',
-      tag: 'Ruby'
-    },
-    {
-      id: 5,
-      title: 'How to check if two hashes are identical?',
-      tag: 'Ruby'
-    },
-    {
-      id: 6,
-      title: 'How to combine two hashes in Ruby?',
-      tag: 'Ruby'
-    },
-    {
-      id: 7,
-      title: 'How to get unique keys from two hashes in Ruby?',
-      tag: 'Ruby'
-    },
-    {
-      id: 8,
-      title: 'How does the has_key? key? member? and include? methods in a hash?',
-      tag: 'Ruby'
-    },
-    {
-      id: 9,
-      title: 'What are blocks in Ruby?',
-      tag: 'Ruby'
-    },
-    {
-      id: 10,
-      title: 'Does the order of keys matters to compare two hashes in Ruby?',
-      tag: 'Ruby'
-    },
-  ]
+  const [questionsList, setQuestionsList] = useState([]);
+  const questionsUrl = 'http://localhost:3000/api/v1/questions';
+
+  const fetchQuestionList = () => {
+    fetch(questionsUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setQuestionsList(data);
+      })
+  }
+
+  useEffect(() => {
+    fetchQuestionList()
+  }, [])
+
+  // const questionsList = [
+  //   {
+  //     id: 1,
+  //     title: 'How to check if a key is present in a hash?',
+  //     tag: 'Ruby'
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'What is the difference between strings and symbol?',
+  //     tag: 'Ruby'
+  //   },
+  //   {
+  //     id: 3,
+  //     title: 'What happen if you add two same keys in hash?',
+  //     tag: 'Ruby'
+  //   },
+  //   {
+  //     id: 4,
+  //     title: 'How to delete a key from a hash?',
+  //     tag: 'Ruby'
+  //   },
+  //   {
+  //     id: 5,
+  //     title: 'How to check if two hashes are identical?',
+  //     tag: 'Ruby'
+  //   },
+  //   {
+  //     id: 6,
+  //     title: 'How to combine two hashes in Ruby?',
+  //     tag: 'Ruby'
+  //   },
+  //   {
+  //     id: 7,
+  //     title: 'How to get unique keys from two hashes in Ruby?',
+  //     tag: 'Ruby'
+  //   },
+  //   {
+  //     id: 8,
+  //     title: 'How does the has_key? key? member? and include? methods in a hash?',
+  //     tag: 'Ruby'
+  //   },
+  //   {
+  //     id: 9,
+  //     title: 'What are blocks in Ruby?',
+  //     tag: 'Ruby'
+  //   },
+  //   {
+  //     id: 10,
+  //     title: 'Does the order of keys matters to compare two hashes in Ruby?',
+  //     tag: 'Ruby'
+  //   },
+  // ]
 
   return (
     <div className='row'>
