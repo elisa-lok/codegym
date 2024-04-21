5c8eb2a4e26805f74d5247475f9df02af240317b
Elisa <elisalok413@gmail.com>
2024-04-21 20:27:07 +1200

    add dislike button to the web page

--- app/javascript/react/src/components/QuestionDetail.jsx
+++ app/javascript/react/src/components/QuestionDetail.jsx
@@ -5,8 +5,12 @@ import * as ReactDOM from 'react-dom';
 class QuestionDetail extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { likeCount: 0 }
-    this.updateLikeCounter = this.updateLikeCounter.bind(this)
+    this.state = {
+      likeCount: 0,
+      dislikeCount: 0
+    }
+    this.updateLikeCounter = this.updateLikeCounter.bind(this);
+    this.updateDislikeCounter = this.updateDislikeCounter.bind(this);
   }
 
   updateLikeCounter() {
@@ -17,19 +21,55 @@ class QuestionDetail extends React.Component {
     })
   }
 
+
+  updateDislikeCounter() {
+    this.setState(function(state) {
+      return {
+        dislikeCount: state.dislikeCount + 1
+      }
+    })
+  }
+
   render() {
     return (
-      <div className='card rounded-0 mt-3'>
-        <div className='card-body'>
-          <h3 className='card-title'>{this.props.question.title}</h3>
-          <p className='lead'>
-            <span className='badge bg-primary'>{this.props.question.tag}</span>
+      <div className="card rounded-0 mt-3">
+        <div className="card-body">
+          <h3 className="card-title">{this.props.question.title}</h3>
+          <p className="lead">
+            <span className="badge bg-primary">{this.props.question.tag}</span>
           </p>
-          <button className='btn btn-primary mt-1' onClick={this.updateLikeCounter}>Like</button>
-          {this.state.likeCount > 0 ? <span className='badge bg-primary'>{this.state.likeCount}</span> : ''}     
+          <button
+            type="button"
+            className="btn btn-primary position-relative"
+            onClick={this.updateLikeCounter} style={{marginRight: 1 + 'em'}}
+          >
+            Like
+              {this.state.likeCount > 0 ? (
+              <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                {this.state.likeCount}
+              </span>
+              ) : (
+                ""
+              )}
+          </button>
+
+          <button
+            type="button"
+            className="btn btn-primary position-relative"
+            onClick={this.updateDislikeCounter}
+          >
+            Dislike
+              {this.state.dislikeCount > 0 ? (
+              <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                {this.state.dislikeCount}
+              </span>
+              ) : (
+                ""
+              )}
+          </button>
         </div>
       </div>
-    )
+    );
   }
 }
 
