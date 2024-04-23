26ec16671eac1160b57701b64bd492cfc0f7f090
Elisa <elisalok413@gmail.com>
2024-04-23 14:37:34 +1200

    fix the bug of removeChild

--- app/javascript/react/src/components/EmptyQuestionMessage.jsx
+++ app/javascript/react/src/components/EmptyQuestionMessage.jsx
@@ -3,14 +3,20 @@ import * as ReactDOM from "react-dom";
 
 const EmptyQuestionMessage = (props) => {
   return (
-    <div className="mt-5 alert alert-danger alert-dismissible fade show" role="alert">
-      <strong>OOPS!</strong> No questions found with the tag: {props.tagname}. Please select another tag.
-      <button
-        type="button"
-        className="btn-close"
-        data-bs-dismiss="alert"
-        aria-label="Close"
-      ></button>
+    <div>
+      <div
+        className="mt-5 alert alert-danger alert-dismissible fade show"
+        role="alert"
+      >
+        <strong>OOPS!</strong> No questions found with the tag: {props.tagname}.
+        Please select another tag.
+        <button
+          type="button"
+          className="btn-close"
+          data-bs-dismiss="alert"
+          aria-label="Close"
+        ></button>
+      </div>
     </div>
   );
 };
